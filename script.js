07f1de2b52f2d80136e46de0058f7dc337f329ac
Kaeste <KaesteCueio@gmail.com>
2026-09-08 21:05:56 -0300

    feat: add js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Elementos
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+const playerChoice = document.getElementById("player-choice");
+const pcChoice = document.getElementById("pc-choice");
+const resultElement = document.getElementById("result");
+
+const resetButton = document.getElementById("reset");
