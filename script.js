b98bd6b91bb8b0c8009c0f1872bf4d6d8207be33
Kaeste <KaesteCueio@gmail.com>
2026-09-08 21:44:56 -0300

    feat: add interactivity

--- script.js
+++ script.js
@@ -3,8 +3,67 @@ const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 
-const playerChoice = document.getElementById("player-choice");
-const pcChoice = document.getElementById("pc-choice");
+const playerChoiceElement = document.getElementById("player-choice");
+const pcChoiceElement = document.getElementById("pc-choice");
 const resultElement = document.getElementById("result");
 
 const resetButton = document.getElementById("reset");
+
+// Jogo
+const translationTable = {
+    "rock": "Pedra",
+    "paper": "Papel",
+    "scissors": "Tesoura",
+    "win": "Você venceu!",
+    "lose": "Você perdeu!",
+    "tie": "Empate!"
+};
+function translate(text) {
+    return translationTable[text];
+}
+
+let player;
+function playerChoice(event) {
+    player = event.target.id;
+    playerChoiceElement.innerHTML = `Você escolheu: <span class="fw-medium">${translate(player)}</span>`;
+
+    rockButton.removeEventListener("click", playerChoice);
+    paperButton.removeEventListener("click", playerChoice);
+    scissorsButton.removeEventListener("click", playerChoice);
+
+    pcChoice();
+}
+
+rockButton.addEventListener("click", playerChoice);
+paperButton.addEventListener("click", playerChoice);
+scissorsButton.addEventListener("click", playerChoice);
+
+let pc;
+function pcChoice() {
+    pc = Math.random();
+    pc = pc < 1/3 ? "rock" : (pc < 2/3 ? "paper" : "scissors");
+    pcChoiceElement.innerHTML = `Computador escolheu: <span class="fw-medium">${translate(pc)}</span>`;
+
+    compareChoices();
+}
+
+const winConditions = {
+    "rock": {
+        "rock": "tie",
+        "paper": "lose",
+        "scissors": "win"
+    },
+    "paper": {
+        "rock": "win",
+        "paper": "tie",
+        "scissors": "lose"
+    },
+    "scissors": {
+        "rock": "lose",
+        "paper": "win",
+        "scissors": "tie"
+    }
+}
+function compareChoices() {
+    resultElement.innerText = translate(winConditions[player][pc]);
+}
